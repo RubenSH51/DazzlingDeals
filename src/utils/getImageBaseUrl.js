3664600b5a63b4d76365fae2db17d41dd24c42cb
RubenSH51 <rubencuello51@gmail.com>
2023-06-23 18:02:51 -0300

    solución al bug de las imagenes tanto para local como para github VOL.2

--- src/utils/getImageBaseUrl.js
+++ src/utils/getImageBaseUrl.js
@@ -1,4 +1,4 @@
 export function getImageBaseUrl() {
     const isLocalhost = window.location.hostname.includes('localhost');
-    return isLocalhost ? 'public' : 'https://github.com/rubensh51/DazzlingDeals';
+    return isLocalhost ? 'public' : 'https://rubensh51.github.io/DazzlingDeals';
   }
